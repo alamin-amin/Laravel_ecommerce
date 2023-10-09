a30466a1079c39a34420435b68ce69e301a89afa
alamin-amin <alaminamin958@gmail.com>
2023-10-10 01:18:25 +0600

    update

---
+++ resources/views/admin/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2023 <a href="https://www.facebook.com/Alamin5001">Md. Alamin</a>.</strong>
+     All rights reserved.
+    <div class="float-right d-none d-sm-inline-block">
+      <b>WDPF</b> R-54
+    </div>
+  </footer>
